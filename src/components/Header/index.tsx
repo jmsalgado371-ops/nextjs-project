3036bf4eae435ca4669353e83049288b41a47e6e
jose salgado <jmsalgado371@gmail.com>
2026-04-17 20:58:26 -0400

    Keep mobile navbar toggle as hamburger icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -142,36 +142,10 @@ const Header = () => {
               aria-expanded={navigationOpen}
               aria-label="Toggle navigation menu"
             >
-              <span className="relative block h-5.5 w-5.5 cursor-pointer">
-                <span className="du-block absolute right-0 h-full w-full">
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-0 duration-200 ease-in-out ${
-                      !navigationOpen ? "w-full! delay-300" : "w-0"
-                    }`}
-                  ></span>
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-150 duration-200 ease-in-out ${
-                      !navigationOpen ? "delay-400 w-full!" : "w-0"
-                    }`}
-                  ></span>
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-white delay-200 duration-200 ease-in-out ${
-                      !navigationOpen ? "w-full! delay-500" : "w-0"
-                    }`}
-                  ></span>
-                </span>
-                <span className="du-block absolute right-0 h-full w-full rotate-45">
-                  <span
-                    className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-white delay-300 duration-200 ease-in-out ${
-                      !navigationOpen ? "h-0! delay-0" : "h-full"
-                    }`}
-                  ></span>
-                  <span
-                    className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-white duration-200 ease-in-out ${
-                      !navigationOpen ? "h-0! delay-200" : "h-0.5"
-                    }`}
-                  ></span>
-                </span>
+              <span className="flex h-5.5 w-5.5 flex-col items-center justify-center gap-[3px]">
+                <span className="block h-0.5 w-full rounded-sm bg-white" />
+                <span className="block h-0.5 w-full rounded-sm bg-white" />
+                <span className="block h-0.5 w-full rounded-sm bg-white" />
               </span>
             </button>
 
